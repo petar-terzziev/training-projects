92132c5e555f31ef615219bc46cfe898b897dbae
petar-terzziev <petar-terzziev@gmail.com>
2018-10-30 11:42:28 +0200

    Added validation

--- petar-terzziev-Python/square_sequence.py
+++ petar-terzziev-Python/square_sequence.py
@@ -1,15 +1,20 @@
 def square(n):
+	if n>32000 or n<0:
+		return 'invalid number'
 	s=''
 	if n==0:
-		return 0
+		return '0'
+
+
+
 	
 
-	for i in range(n+1):
+	for i in range(n+1) :
 		s+=str(i**2)
 	
 
 	return s[n]
 
 
-no=square(0)
+no=square()
 print(no)
