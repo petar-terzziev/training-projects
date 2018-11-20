d687f07c831d8104c6faeade4fe0b98f8ad13424
petar-terzziev <petar-terzziev@gmail.com>
2018-11-20 17:17:32 +0200

    almost got bfs to work

--- petar-terzziev-Maze/maze.py
+++ petar-terzziev-Maze/maze.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+from queue import *
+from collections import namedtuple
 matrix = [
 [' ', ' ', ' ', '*', ' ', ' ', ' '],
 ['*', '*', ' ', '*', ' ', '*', ' '],
@@ -9,16 +10,18 @@ matrix = [
 ]
 
 
+Point = namedtuple('Point', 'x y')
+qnode=namedtuple('qnode','point distance ')
 
 def search(x,y):
 	if matrix[x][y]=='e':
 		print("We are out!")
 		return True
 	elif matrix[x][y]=='*':
-		print("Wall at %d %d"%(x,y))
+	
 		return False
 	elif matrix[x][y]=='.':
-		print("Visited at %d %d" %(x,y))
+		
 		return False
 	print("visiting %d %d"%(x,y))
 
@@ -32,4 +35,45 @@ def search(x,y):
 		return True
 	return False
 
-search(0,0)
+rowit = [-1, 0, 0, 1]; 
+colit = [0, -1, 1, 0]; 
+valid=lambda x,y: (x<=len(matrix)-1 and y<=len(matrix[0])-1 and x>=0 and y>=0)
+start=Point(0,0)
+sn=qnode(start,0)
+matrix[0][0]='.'
+q=[]
+q.append(sn)
+def bfs():
+	visited=[[False*len(matrix)]*len(matrix[0])]
+	while len(q)>0:
+
+		curr=q[0]
+		print(curr)
+		if(curr[0][0]==4 and curr[0][1]==6):
+			return curr[1]
+
+		q.pop(0)
+	
+		for i in range(4):
+			row=curr[0][0]+rowit[i]
+			col=curr[0][1]+colit[i]
+			if(valid(row,col) and matrix[row][col]==' ' and (not visited[row][col])):
+				visited[row][col]=True
+				adjc=qnode(Point(row,col),curr[1]+1)
+				
+				q.append(adjc)
+
+
+
+
+okok=bfs()
+print(okok)
+	
+
+
+
+#search(0,0)
+#for i in range(5):
+#	for j in range(7):
+#		print(matrix[i][j]+" ",end='',flush=True)
+#	print (' ')
